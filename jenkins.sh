b0562d3280e96197fb2bd97eb7e70f425030bdcd
Hanno Schlichting <hanno@hannosch.eu>
2013-02-27 19:33:03 +0100

    extend jenkins driver to start ES/Pyramid

--- jenkins.sh
+++ jenkins.sh
@@ -8,9 +8,24 @@ find . -name '*.pyc' -exec rm {} \;
 
 make build
 
+echo "Starting ElasticSearch and Pyramid..."
+
+elasticsearch/bin/elasticsearch -p es.pid
+bin/pserve --pid-file monolith.pid --daemon monolith.ini
+sleep 5
+
 echo "Starting tests..."
 
 bin/nosetests -s -d -v --with-xunit --with-coverage --cover-package monolith monolith
+
+echo "Stopping ElasticSearch and Pyramid"
+kill `cat es.pid`
+kill `cat monolith.pid`
+rm -f es.pid
+rm -f monolith.pid
+
+echo "Calculating coverage..."
+
 bin/coverage xml $(find monolith/client -name '*.py')
 
 echo "FIN"

8a590f70e5f38755af0d93900bd0d4f31bf100e3
Hanno Schlichting <hanno@hannosch.eu>
2013-03-04 17:07:25 +0100

    give ES server up to 30 seconds to start and check connection instead of doing a simple `time.sleep(5)` and hoping for the best

--- runtests.py
+++ runtests.py
@@ -2,6 +2,26 @@ import os
 import sys
 import time
 
+from pyelasticsearch import ElasticSearch
+
+
+def wait_until_es_is_ready():
+    client = ElasticSearch('http://localhost:9213')
+    time.sleep(2)
+    now = time.time()
+    while time.time() - now < 30:
+        try:
+            # check to see if our process is ready
+            health = client.health()
+            status = health['status']
+            name = health['cluster_name']
+            if status == 'green' and name == 'monotest':
+                return 0
+        except Exception:
+            # wait a bit before re-trying
+            time.sleep(0.5)
+    return 1
+
 
 def main():
     ret = {}
@@ -12,10 +32,11 @@ def main():
             'elasticsearch/bin/elasticsearch -p es.pid')
         ret['monolith_start'] = os.system(
             'bin/pserve --pid-file monolith.pid --daemon monolith.ini')
-        time.sleep(5)
-        ret['test'] = os.system(
-            'bin/nosetests -s -d -v --with-xunit --with-coverage '
-            '--cover-package monolith monolith')
+        ret['es_client'] = wait_until_es_is_ready()
+        if not any(ret.values()):
+            ret['test'] = os.system(
+                'bin/nosetests -s -d -v --with-xunit --with-coverage '
+                '--cover-package monolith monolith')
     finally:
         ret['es_stop'] = os.system('kill `cat es.pid`')
         ret['monolith_stop'] = os.system('kill `cat monolith.pid`')

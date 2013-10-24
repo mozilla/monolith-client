e5ab292cf13891ec604fd74ff9d7c50d79bcb292
Rob Hudson <chudson@mozilla.com>
2013-10-24 12:03:31 -0700

    Added virtualenv creation to jenkins script

--- jenkins.sh
+++ jenkins.sh
@@ -3,10 +3,20 @@ set -e
 
 echo "Starting build on executor $EXECUTOR_NUMBER..."
 
+cd $WORKSPACE
+VENV=$WORKSPACE/venv
+ES_HOST='jenkins-es20:9200'
+
 # Make sure there's no old pyc files around.
 find . -name '*.pyc' -exec rm {} \;
 
-ES_HOST='jenkins-es20:9200'
+# Handle virtualenv creation.
+if [ ! -d "$VENV/bin" ]; then
+  echo "No virtualenv found.  Making one..."
+  virtualenv $VENV --system-site-packages
+fi
+source $VENV/bin/activate
+pip install -U --exists-action=w --no-deps -q -r requirements/prod.txt -r requirements/test.txt
 
 echo "Running tests"
 
